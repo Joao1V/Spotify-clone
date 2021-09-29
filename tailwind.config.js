b9257cc54d49c9e391d377ae60bc04957509d3e9
JoaoV <joao.vfm18@gmail.com>
2021-09-29 13:57:29 -0400

    Criando NavBar

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        14: "3.5rem",
+        "125px": "125px",
+      },
+      colors: {
+        hoverspt: "#18D760",
+        "purple-main": "#2DA6B9",
+        "green-main": "#1ED760",
+      },
+      backgroundImage: (theme) => ({
+        "spotify-theme": "url('/imgs/bursts.svg')",
+        "spotify-theme-mobile": "url('/imgs/bursts-mobile.svg')",
+      }),
+      backgroundSize: {
+        "175%": "175%",
+        "195%": "195%",
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center',
+      },
+      fontSize:{
+        '9xl':'9rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
